53a2acbedc56860326443befb646c3b3ae82cc96
Cassie Wilcox <cassiewilcox@Cassies-MacBook-Pro.local>
2015-09-28 20:14:39 -0400

    linting code

--- component/ngProperPossessive.directive.js
+++ component/ngProperPossessive.directive.js
@@ -8,8 +8,8 @@ angular.module('properPossessive')
                 function properize(name) {
                     var ending = name.substr(name.length-1) !== 's' ? 's' : '';
                     return name + '\'' + ending;
-                };
+                }
                 element.text(properize(attrs.name));
             }
-        }
+        };
     });
