c129817b631fca33124fa28941b0f9c32bd62bd6
Cassie Wilcox <csndra0504@gmail.com>
2015-09-28 21:33:14 -0400

    replaced dist scripts

---
+++ dist/ngProperPosessive.js
@@ -0,0 +1,25 @@
+/*!
+ * See LICENSE in this repository for license information
+ */
+(function(){
+'use strict';
+
+angular
+  .module('ngProperPossessive', []);
+
+'use strict';
+
+angular.module('ngProperPossessive')
+    .directive('properPossessive', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                function properize(name) {
+                    var ending = name.substr(name.length-1) !== 's' ? 's' : '';
+                    return name + '\'' + ending;
+                }
+                element.text(properize(attrs.name));
+            }
+        };
+    });
+})();
